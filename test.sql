15f30b8890c89e9f087b5c25bdd4ed4231cd6f76
Taishiro Inokuchi <tai0721abc@gmail.com>
2021-02-13 20:19:28 +0900

    :tada: initial commit

---
+++ test.sql
@@ -0,0 +1,11 @@
+-- create table shohin
+-- (
+-- 	shohin_id char(4) not null,
+-- 	shohin_mei varchar(100),
+-- 	shohin_bunrui varchar(32),
+-- 	hanbai_tanka integer,
+-- 	shiire_tanka integer,
+-- 	torokubi date,
+-- 	primary key (shohin_id)
+-- );
+
