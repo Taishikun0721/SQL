70477c2c85d8497a2f2ad83d19824286edd040b2
Taishiro Inokuchi <tai0721abc@gmail.com>
2021-02-22 23:42:54 +0900

    :tada: 達人に学ぶやつ開始

--- test.sql
+++ test.sql
@@ -719,3 +719,33 @@ from categories as c
 inner join foods as f
 on c.id = f.category_id
 group  by c.name;
+
+select  case pref_name
+	when '徳島' then '四国'
+	when '香川' then '四国'
+	when '愛媛' then '四国'
+	when '高知' then '四国'
+	when '福岡' then '九州'
+	when '佐賀' then '九州'
+	when '長崎' then '九州'
+	else 'その他'
+end as district, sum(population)
+from PopTbl
+group by district;
+
+
+select case
+	when population < 100 then '01'
+	when population >= 100 and population < 200 then '02'
+	when population >= 200 and population < 300 then '03'
+	when population >= 300 then '04'
+	else null end as pop_class,
+	count(population)
+from PopTbl
+group by pop_class
+order by pop_class;
+
+select pref_name, sum(case when sex = '1' then population else 0 end) as cnt_m,
+sum(case when sex = '2' then population else 0 end) as cnt_f
+from PopTbl2
+group by pref_name;
