3f740881bf8dc384596769a484bada7386f91248
Taishiro Inokuchi <tai0721abc@gmail.com>
2021-02-27 00:37:57 +0900

    :sparkles: 主キーを操作するSQL

--- test.sql
+++ test.sql
@@ -831,3 +831,12 @@ set salary = case when salary >= 300000
 			then salary * 1.2
 			else salary end;
 
+
+update SomeTable
+set p_key = case when p_key = 'a'
+			then 'b'
+			when p_key = 'b'
+			then 'a'
+			else p_key end
+where p_key in ('a', 'b');
+
